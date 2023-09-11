86ad6e837442278a6ba1150f880e04ea95877f8e
Golovanov Artyom <golovanov.a@docsvision.com>
2023-09-11 17:12:01 +0300

    InsertGreetingExtension

---
+++ InsertGreetingExtension/src/Index.ts
@@ -0,0 +1,10 @@
+﻿import * as InsertGreeting from "./InsertGreeting";
+import { extensionManager } from "@docsvision/webclient/System/ExtensionManager";
+
+
+
+extensionManager.registerExtension({
+    name: "Insert greeting extension",
+    version: "2.0",
+    globalEventHandlers: [InsertGreeting ]
+})
