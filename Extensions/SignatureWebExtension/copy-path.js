33ac77762cc93f6981ddd5308aec6ae7e8e29261
Olga Baranova <baranova.ol@docsvision.com>
2023-09-20 10:44:14 +0300

    TSK-5007 КЭДО. Поправить сборку расширений

--- Extensions/SignatureWebExtension/copy-path.js
+++ Extensions/SignatureWebExtension/copy-path.js
@@ -1,6 +1,6 @@
 const SITE_ROOT = "Dist/Site";
 const EXTENSION_NAME = "Signature";
-const MODULES_DIR = SITE_ROOT + "/Content/Modules";
+const MODULES_DIR = SITE_ROOT + "/Content/Modules/KEDO";
 
 module.exports.STYLES_DIR =  `${MODULES_DIR}/${EXTENSION_NAME}`;
 module.exports.BUNDLE_DIR =  `${MODULES_DIR}/${EXTENSION_NAME}`;
