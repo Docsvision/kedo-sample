03cace96dddf6e4821795f9bc7de7f1072da7f9d
Olga Baranova <baranova.ol@docsvision.com>
2023-10-05 17:51:11 +0300

    ERR-5682 Обычному пользователю не завершить задачу на подписание КЭДО

--- Extensions/SignatureWebExtension/src/EventHandlers.tsx
+++ Extensions/SignatureWebExtension/src/EventHandlers.tsx
@@ -1,23 +1,33 @@
-﻿import { $DigitalSignature, BeforeSignatureDialogShowEventArgs } from "@docsvision/webclient/BackOffice/$DigitalSignature";
+﻿import { $DigitalSignature, AfterSignatureDialogShowEventArgs, BeforeSignatureDialogShowEventArgs } from "@docsvision/webclient/BackOffice/$DigitalSignature";
 import { $StaffDirectoryItemsController } from "@docsvision/webclient/Generated/DocsVision.WebClient.Controllers";
+import { MessageBox } from "@docsvision/webclient/Helpers/MessageBox/MessageBox";
 import { $ApplicationSettings } from "@docsvision/webclient/StandardServices";
 import { Layout } from "@docsvision/webclient/System/Layout";
+import { resources } from "@docsvision/webclient/System/Resources";
 
 
 
 export async function signature(sender: Layout) {
-
     const { digitalSignature } = sender.layout.getService<$DigitalSignature>()
 
     const userId = sender.layout.getService($ApplicationSettings).employee.id
     const groups = await sender.layout.getService($StaffDirectoryItemsController).getEmployeeGroups(userId)
 
     digitalSignature.beforeSignatureDialogShown.watch((data: BeforeSignatureDialogShowEventArgs) => {
-
-
         if (groups.find(el => el.id === '6dc6944c-7dca-4861-b3cd-80a192df45cc')) {
-            data.dialogProps.hideLabelSelection = true
-            data.dialogProps.hideSignatureMethodsSetup = true
+            data.dialogProps.hideLabelSelection = true;
+            data.dialogProps.hideSignatureMethodsSetup = true;
+            data.dialogProps.showHidden = true;;
+        }
+    })
+
+    digitalSignature.afterSignatureDialogShown.watch(async (data: AfterSignatureDialogShowEventArgs) => {
+        const methods = await data.management.getSignatureMethods();
+        if((!methods || methods.length === 0 || !methods.find(m => m?.isUserDefault))) {
+            data.management.close();
+            MessageBox.ShowError(resources.DefaultCertificateError);
+        } else {
+            data.management.setDialogVisible(true);
         }
     })
 }
--- Extensions/SignatureWebExtension/src/Index.ts
+++ Extensions/SignatureWebExtension/src/Index.ts
@@ -1,5 +1,6 @@
 ﻿import * as EventHandlers from "./EventHandlers";
 import { extensionManager } from "@docsvision/webclient/System/ExtensionManager";
+import { ILocalizationsMap } from "@docsvision/webclient/System/ILocalizationsMap";
 
 
 // Главная входная точка всего расширения
@@ -11,5 +12,17 @@ import { extensionManager } from "@docsvision/webclient/System/ExtensionManager"
 extensionManager.registerExtension({
     name: "Signature",
     version: "1.0",
-    globalEventHandlers: [ EventHandlers ]
+    globalEventHandlers: [ EventHandlers ],
+    getLocalizations
 })
+
+function getLocalizations(): ILocalizationsMap {
+    let cultureMap = {};
+    cultureMap["ru"] = {
+        "DefaultCertificateError": "Не настроен сертификат по умолчанию. Обратитесь к администратору."
+    };
+    cultureMap["en"] = {
+        "DefaultCertificateError": "The default certificate is not configured. Contact your administrator.",
+    };
+    return cultureMap;
+}
