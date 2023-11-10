89de5617f4f9ae49c8a9ecbed842532c966b6837
Olga Baranova <baranova.ol@docsvision.com>
2023-11-10 13:44:24 +0300

    ERR-6032 КЭДО. При подписании кадрового документа не должно быть возможности выбора сертификата

--- Extensions/KEDOWebExtension/src/EventHandlers.tsx
+++ Extensions/KEDOWebExtension/src/EventHandlers.tsx
@@ -14,22 +14,25 @@ export async function signature(sender: Layout) {
 
     const userId = sender.layout.getService($ApplicationSettings).employee.id
     const groups = await sender.layout.getService($StaffDirectoryItemsController).getEmployeeGroups(userId)
+    const isKEDOUser = groups.find(el => el.id === '6dc6944c-7dca-4861-b3cd-80a192df45cc');
 
     digitalSignature.beforeSignatureDialogShown.watch((data: BeforeSignatureDialogShowEventArgs) => {
-        if (groups.find(el => el.id === '6dc6944c-7dca-4861-b3cd-80a192df45cc')) {
-            data.dialogProps.hideLabelSelection = true;
-            data.dialogProps.hideSignatureMethodsSetup = true;
-            data.dialogProps.showHidden = true;;
-        }
+        if (!isKEDOUser) { return; }
+        data.dialogProps.hideLabelSelection = true;
+        data.dialogProps.hideSignatureMethodsSetup = true;
+        data.dialogProps.showHidden = true;
     })
 
     digitalSignature.afterSignatureDialogShown.watch(async (data: AfterSignatureDialogShowEventArgs) => {
+        if (!isKEDOUser) { return; }
         const methods = await data.management.getSignatureMethods();
-        if((!methods || methods.length === 0 || !methods.find(m => m?.isUserDefault))) {
+        const defaultMethod = methods && methods.find(m => m?.isUserDefault);
+        if(!defaultMethod) {
             data.management.close();
             MessageBox.ShowInfo(resources.DefaultCertificateError);
         } else {
-            data.management.setDialogVisible(true);
+            data.management.selectSignatureMethod(defaultMethod);
+            data.management.accept();
         }
     })
 }
