0fbdd3b82bdda6118c0272c065d5a058370a75a8
Kristina Peshne <peshne.k@docsvision.com>
2023-11-29 13:15:51 +0300

    ERR-6060 КЭДО. При переоткрытии карточки с открытием диалога подписания события beforeSignatureDialogShown и afterSignatureDialogShown начинаются дублироваться

--- Extensions/KEDOWebExtension/src/InitExtension.ts
+++ Extensions/KEDOWebExtension/src/InitExtension.ts
@@ -11,22 +11,22 @@ export function initExtension(app: IApp & $StandardControllers & $StandardServic
     const digitalSignature = app.getService($DigitalSignature);
 
     digitalSignature.beforeSignatureDialogShown.watch((data: BeforeSignatureDialogShowEventArgs) => {
-        if(!data.cardInfo || data.cardInfo.kindId !== "028f603f-e8ba-4427-93e6-f31953a00c7f") { return; }
+        if (!data.cardInfo || data.cardInfo.kindId !== "028f603f-e8ba-4427-93e6-f31953a00c7f") { return; }
         data.dialogProps.showHidden = true;
     })
 
-    digitalSignature.afterSignatureDialogShown.watch(async (data: AfterSignatureDialogShowEventArgs) => {   
-        if(!data.cardInfo || data.cardInfo.kindId !== "028f603f-e8ba-4427-93e6-f31953a00c7f") { return; }
-        const userId = app.getService($ApplicationSettings).employee.id
-        const groups = await app.getService($StaffDirectoryItemsController).getEmployeeGroups(userId)
+    digitalSignature.afterSignatureDialogShown.watch(async (data: AfterSignatureDialogShowEventArgs) => {
+        if (!data.cardInfo || data.cardInfo.kindId !== "028f603f-e8ba-4427-93e6-f31953a00c7f") { return; }
+        const userId = app.getService($ApplicationSettings).employee.id;
+        const groups = await app.getService($StaffDirectoryItemsController).getEmployeeGroups(userId);
         const isKEDOUser = groups.find(el => el.id === '6dc6944c-7dca-4861-b3cd-80a192df45cc');
-        if (!isKEDOUser) { 
+        if (!isKEDOUser) {
             data.management.setDialogVisible(true);
-            return; 
+            return;
         }
         const methods = await data.management.getSignatureMethods();
         const defaultMethod = methods && methods.find(m => m?.isUserDefault);
-        if(!defaultMethod) {
+        if (!defaultMethod) {
             data.management.close();
             MessageBox.ShowInfo(resources.DefaultCertificateError);
         } else {
